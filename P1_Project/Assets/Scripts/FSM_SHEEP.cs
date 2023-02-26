d6675ce8bc2c70f70e3b65c15cb6199ea5f3a29e
agcarbo19 <agcarbo19@users.noreply.github.com>
2023-02-26 11:30:33 +0100

    me encanta unity

--- P1_Project/Assets/Scripts/FSM_SHEEP.cs
+++ P1_Project/Assets/Scripts/FSM_SHEEP.cs
@@ -5,9 +5,6 @@ using Steerings;
 [CreateAssetMenu(fileName = "FSM_SHEEP", menuName = "Finite State Machines/FSM_SHEEP", order = 1)]
 public class FSM_SHEEP : FiniteStateMachine
 {
-    /* Declare here, as attributes, all the variables that need to be shared among
-     * states and transitions and/or set in OnEnter or used in OnExit 
-     * For instance: steering behaviours, blackboard, ...*/
     private BLACKBOARD_SHEEP blackboardSheep;
     private FlockingAround flockingAround;
     private Flee flee;
@@ -16,10 +13,6 @@ public class FSM_SHEEP : FiniteStateMachine
 
     public override void OnEnter()
     {
-        /* Write here the FSM initialization code. This code is execute every time the FSM is entered.
-         * It's equivalent to the on enter action of any state 
-         * Usually this code includes .GetComponent<...> invocations */
-
         blackboardSheep = GetComponent<BLACKBOARD_SHEEP>();
         flockingAround = GetComponent<FlockingAround>();
         flee = GetComponent<Flee>();
@@ -29,27 +22,13 @@ public class FSM_SHEEP : FiniteStateMachine
 
     public override void OnExit()
     {
-        /* Write here the FSM exiting code. This code is execute every time the FSM is exited.
-         * It's equivalent to the on exit action of any state 
-         * Usually this code turns off behaviours that shouldn't be on when one the FSM has
-         * been exited. */
-
         base.DisableAllSteerings();
         base.OnExit();
     }
 
     public override void OnConstruction()
     {
-        /* STAGE 1: create the states with their logic(s)
-         *-----------------------------------------------
-         
-        State varName = new State("StateName",
-            () => { }, // write on enter logic inside {}
-            () => { }, // write in state logic inside {}
-            () => { }  // write on exit logic inisde {}  
-        );
-
-        */
+        // STAGE 1: create the states with their logic(s)
 
         State walkAround = new State("WalkAround",
             () => {
@@ -69,15 +48,7 @@ public class FSM_SHEEP : FiniteStateMachine
             () => { flee.enabled = false; }
         );
 
-        /* STAGE 2: create the transitions with their logic(s)
-         * ---------------------------------------------------
-
-        Transition varName = new Transition("TransitionName",
-            () => { }, // write the condition checkeing code in {}
-            () => { }  // write the on trigger code in {} if any. Remove line if no on trigger action needed
-        );
-
-        */
+        // STAGE 2: create the transitions with their logic(s)
 
         Transition wolfNearby = new Transition("Wolf Nearby",
             () => {
@@ -90,24 +61,13 @@ public class FSM_SHEEP : FiniteStateMachine
             () => { return SensingUtils.DistanceToTarget(gameObject, wolf) > blackboardSheep.wolfFarAwayRadius; }
         );
 
-        /* STAGE 3: add states and transitions to the FSM 
-         * ----------------------------------------------
-            
-        AddStates(...);
-
-        AddTransition(sourceState, transition, destinationState);
-
-        */
+        // STAGE 3: add states and transitions to the FSM 
 
         AddStates(walkAround, runAway);
         AddTransition(walkAround, wolfNearby, runAway);
         AddTransition(runAway, wolfFarAway, walkAround);
 
-        /* STAGE 4: set the initial state
-         
-        initialState = ... 
-
-        */
+        // STAGE 4: set the initial state
 
         initialState = walkAround;
     }
